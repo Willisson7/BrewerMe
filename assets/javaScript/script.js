3936f7cf04cec0e3c75d7fe20f4bf5d22883f2d0
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-02-12 14:02:03 -0700

    Getting Closer

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -6,51 +6,57 @@ var stateInput = document.querySelector('#stateInput')
 
 //Find City/Brewery Input
 function coordinates(city) {
-    var brew1 = document.createElement('button')
-    var brewPhone = document.createElement('p')
-    var brew2 = document.createElement('button')
-    var brew3 = document.createElement('button')
-    var brew4 = document.createElement('button')
-    var brew5 = document.createElement('button')
+    // var brew1 = document.createElement('button')
+    // var brewPhone = document.createElement('p')
+    // var brew2 = document.createElement('button')
+    // var brew3 = document.createElement('button')
+    // var brew4 = document.createElement('button')
+    // var brew5 = document.createElement('button')
     fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityInput.value.trim()}&by_state=${stateInput.value.trim()}&per_page=5`)
         .then((response) => response.json())
         .then((data) => {
             document.querySelector('.brewName').innerHTML = "";
-            // for (var i = 0; i < data.length; i++) {
-            //     brewInfo();
-            // }
+            for (var i = 0; i < data.length; i++) {
+             let results = brewInfo(data[i]);
+
+             console.log("look here!", results)
+             
+                
 
 
-                console.log('Brewery', data)
+
+
+            }
+            console.log('Brewery', data)
 
             // check to see if data is null
             if (data && data.length != 0) {
                 currentWeather(data[0].name)
                 console.log(data[0].city)
-                brew1.textContent = data[0].name
-                document.querySelector('#brew1').append(brew1);
-                brewPhone.textContent = data[0].phone
-                // brewPhone.classList.add('.none')
-                // brewName.addEventListener("click", () => brewPhone.classList = "")
-                document.querySelector('.brewName').append(brewPhone)
-                
-                brew1.textContent = data[0].name
-                document.querySelector('#brew1').append(brew1);
+                // brew1.textContent = data[0].name
+                // document.querySelector('#brew1').append(brew1);
+                // brewPhone.textContent = data[0].phone
+                // // brewPhone.classList.add('.none')
+                // // brewName.addEventListener("click", () => brewPhone.classList = "")
+                // document.querySelector('.brewName').append(brewPhone)
 
-                brew1.textContent = data[0].website_url
-                document.querySelector('.brewDescription')
+                // brew1.textContent = data[0].name
+                // document.querySelector('#brew1').append(brew1);
 
-                brew2.textContent = data[1].name
-                document.querySelector('#brew2').append(brew2)
-                
-                brew3.textContent = data[2].name
-                document.querySelector('#brew3').append(brew3)
+                // brew1.textContent = data[0].website_url
+                // document.querySelector('.brewDescription').append(brew1)
 
-                brew4.textContent = data[3].name
-                document.querySelector('#brew4').append(brew4)
+                // brew2.textContent = data[1].name
+                // document.querySelector('#brew2').append(brew2)
 
-                brew5.textContent = data[4].name
-                document.querySelector('#brew5').append(brew5)
+                // brew3.textContent = data[2].name
+                // document.querySelector('#brew3').append(brew3)
+
+                // brew4.textContent = data[3].name
+                // document.querySelector('#brew4').append(brew4)
+
+                // brew5.textContent = data[4].name
+                // document.querySelector('#brew5').append(brew5)
 
 
 
@@ -64,15 +70,27 @@ function coordinates(city) {
         );
 }
 //function to return brewInfo, url, street address, and phone number
-// function brewInfo(data) {
-//     info = data['website_url']
-//     info.textContent = data[i];
+function brewInfo(dataElement) {
+brewClass = document.querySelector(".brewName")
+ brewName = dataElement.name
+ brewPhone = dataElement.phone
+ brewWeb = dataElement.website_url
 
-//     document.querySelector('.brewName').append(info);
-// }
-//Pass cityInput into weather fetch url
+
+
+ document.querySelector(".brewName").append(brewName, brewPhone, brewWeb)
+// console.log("Look Here", dataElement.name, dataElement.phone, dataElement.website_url)
+
+}
+
+
+
+
+
+
+
+// Pass cityInput into weather fetch url
 function currentWeather(city) {
-    console.log("coordinates")
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${weatherAPI}`)
 
         .then((response) => response.json())
@@ -84,39 +102,39 @@ function currentWeather(city) {
             var sunnyImg = document.getElementById("sunnyPic")
             var cloudyImg = document.getElementById("cloudyPic")
             console.log(clouds)
-            if(clouds == "clear sky"){
+            if (clouds == "clear sky") {
                 suggested.textContent = "The sun is out! So are you drinking a beer yet?"
                 sunnyImg.classList.remove("hidden")
             }
-            else if(clouds == "few clouds"){
+            else if (clouds == "few clouds") {
                 suggested.textContent = "The sun is out! So are you drinking a beer yet?"
                 sunnyImg.classList.remove("hidden")
             }
-            else if(clouds == "scattered clouds"){
+            else if (clouds == "scattered clouds") {
                 suggested.textContent = "Looks like the sun should be peaking through, and there should be a nice cold beer in your hand!"
                 cloudyImg.classList.remove("hidden")
             }
-            else if(clouds == "broken clouds"){
+            else if (clouds == "broken clouds") {
                 suggested.textContent = "Looks like the sun should be peaking through, and there should be a nice cold beer in your hand!"
                 cloudyImg.classList.remove("hidden")
             }
-            else if(clouds == "shower rain") {
+            else if (clouds == "shower rain") {
                 suggested.textContent = "Might want to take a rain check... SIKE it is just water! Safely go out and grab a nice brewsky!"
                 rainImg.classList.remove("hidden")
             }
-            else if (clouds == "rain"){
+            else if (clouds == "rain") {
                 suggested.textContent = "Might want to take a rain check... SIKE it is just water! Safely go out and grab a nice brewsky!"
                 rainImg.classList.remove("hidden")
             }
-            else if(clouds == "thunderstorm"){
+            else if (clouds == "thunderstorm") {
                 suggested.textContent = "Might want to take a rain check... SIKE it is just water! Safely go out and grab a nice brewsky!"
                 rainImg.classList.remove("hidden")
             }
-            else if(clouds =="snow"){
+            else if (clouds == "snow") {
                 suggested.textContent = "Might want to take a rain check... SIKE it is just water! Safely go out and grab a nice brewsky!"
                 rainImg.classList.remove("hidden")
             }
-            else{
+            else {
                 suggested.textContent = "Might want to take a rain check... SIKE it is just water! Safely go out and grab a nice brewsky!"
                 rainImg.classList.remove("hidden")
             }
