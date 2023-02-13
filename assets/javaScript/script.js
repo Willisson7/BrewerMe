a5ea8522a6d0b3b21578b37c146248d3a8f0ecca
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-02-12 20:04:55 -0700

    Got info displaying to screen

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -6,26 +6,13 @@ var stateInput = document.querySelector('#stateInput')
 
 //Find City/Brewery Input
 function coordinates(city) {
-    // var brew1 = document.createElement('button')
-    // var brewPhone = document.createElement('p')
-    // var brew2 = document.createElement('button')
-    // var brew3 = document.createElement('button')
-    // var brew4 = document.createElement('button')
-    // var brew5 = document.createElement('button')
     fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityInput.value.trim()}&by_state=${stateInput.value.trim()}&per_page=5`)
         .then((response) => response.json())
         .then((data) => {
             document.querySelector('.brewName').innerHTML = "";
             for (var i = 0; i < data.length; i++) {
-                // let results = brewInfo(data[i]);
-
-                // console.log("look here!", results)
-
-
-
-
-
-
+                let results = brewInfo(data[i]);
+                // console.log("look here!", results
             }
             console.log('Brewery', data)
 
@@ -33,35 +20,6 @@ function coordinates(city) {
             if (data && data.length != 0) {
                 currentWeather(data[0].name)
                 console.log(data[0].city)
-                // brew1.textContent = data[0].name
-                // document.querySelector('#brew1').append(brew1);
-                // brewPhone.textContent = data[0].phone
-                // // brewPhone.classList.add('.none')
-                // // brewName.addEventListener("click", () => brewPhone.classList = "")
-                // document.querySelector('.brewName').append(brewPhone)
-
-                // brew1.textContent = data[0].name
-                // document.querySelector('#brew1').append(brew1);
-
-                // brew1.textContent = data[0].website_url
-                // document.querySelector('.brewDescription').append(brew1)
-
-                // brew2.textContent = data[1].name
-                // document.querySelector('#brew2').append(brew2)
-
-                // brew3.textContent = data[2].name
-                // document.querySelector('#brew3').append(brew3)
-
-                // brew4.textContent = data[3].name
-                // document.querySelector('#brew4').append(brew4)
-
-                // brew5.textContent = data[4].name
-                // document.querySelector('#brew5').append(brew5)
-
-
-
-
-
             }
             else {
                 alert('City Not Found')
@@ -78,21 +36,11 @@ function brewInfo(dataElement) {
 
     var line = document.createElement('p')
     line.className = "line";
-    line.innerHTML = brewName + brewPhone + brewWeb;
+    line.innerHTML = brewName + "<br>" + brewPhone + "<br>" + brewWeb;
 
-
-
-    document.querySelector(".brewName").append(line)
+    document.querySelector(".brewName").append(line);
     // console.log("Look Here", dataElement.name, dataElement.phone, dataElement.website_url)
-
 }
-
-
-
-
-
-
-
 // Pass cityInput into weather fetch url
 function currentWeather(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${weatherAPI}`)
@@ -145,10 +93,6 @@ function currentWeather(city) {
         }
         );
 }
-
-
-
-
 submit.addEventListener('click', function () {
 
     var input = document.querySelector('.searchBox').value
